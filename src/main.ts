3d253195d366505b83503ce4ad9baf990081b0f7
taejun.park <taejun.park@yanolja.com>
2023-04-21 15:29:57 +0900

    feat: 카메라 컨트롤 추가

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-console.log('main');
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
@@ -14,6 +14,8 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
